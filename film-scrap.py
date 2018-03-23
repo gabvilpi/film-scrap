3dd5756dda687cedf4e5496e8e3e1699a481ee6a
gabvilpi <gabvilpi@gmail.com>
2018-03-23 20:01:09 +0100

    Se incluye el for que recorrerá la sección de "Todas las películas"

--- film-scrap.py
+++ film-scrap.py
@@ -1,7 +1,12 @@
 import urllib
 import urllib.request
 import urllib.error
+import itertools
 
+'''
+La función download descarga una web indicando el nombre del agente y el número de reintentos.
+Se realiza un tratamiento de las excepciones para mostrar el error o reintentar la descarga si el error es de tipo 5XX
+'''
 def download(url, user_agent="gabvilpi", num_retries=2):
     print("Downloading:", url)
     headers = {'User-agent': user_agent}
@@ -17,7 +22,16 @@ def download(url, user_agent="gabvilpi", num_retries=2):
                 return download(url, user_agent, num_retries-1)
     return html
 
-url="https://www.filmaffinity.com/es/film161026.html"
-
-page = download(url)
-print(page.read())
+# list contiene el orden alfabético que se recorrerá en la sección "Todas las películas" de filmaffinity
+list = ("0-9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+# testList es un listado de prueba
+testList = ("X")
+# Recorremos la sección de todas las películas según list e itertools.cout hasta llegar a una página que no existe
+for i in testList:
+    for num in itertools.count(1):
+        url="https://www.filmaffinity.com/es/allfilms_" + i + "_" + str(num) + ".html"
+        page = download(url)
+        if page is None:
+            break
+        else:
+            print(page.read())
