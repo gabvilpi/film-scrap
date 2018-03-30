120b92255b548c20864fa07793640fb350a1138e
miguelrodriguezolmos <37781686+miguelrodriguezolmos@users.noreply.github.com>
2018-03-31 00:15:20 +0200

    Update film-scrap.py

--- film-scrap.py
+++ film-scrap.py
@@ -4,11 +4,9 @@ import urllib.error
 import itertools
 import re
 from bs4 import BeautifulSoup
+import pandas as pd
 
-'''
-La función download descarga una web indicando el nombre del agente y el número de reintentos.
-Se realiza un tratamiento de las excepciones para mostrar el error o reintentar la descarga si el error es de tipo 5XX
-'''
+# definicion de funcion para bajar peliculas
 def download(url, user_agent="gabvilpi", num_retries=2):
     print("Downloading:", url)
     headers = {'User-agent': user_agent}
@@ -21,17 +19,29 @@ def download(url, user_agent="gabvilpi", num_retries=2):
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 # Reintento para errores HTTP 5XX
-                return download(url, user_agent, num_retries-1)
+                return download(url, user_agent, num_retries - 1)
     return html
 
-# list contiene el orden alfabético que se recorrerá en la sección "Todas las películas" de filmaffinity
-list = ("0-9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+# movielist contiene el orden alfabético que se recorrerá en la sección "Todas las películas" de filmaffinity
+movielist = (
+"0-9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+"W", "X", "Y", "Z")
+
 # testList es un listado de prueba
 testList = ("X")
+testList2 = ("X", "Y")
+
 # Recorremos la sección de todas las películas según list e itertools.cout hasta llegar a una página que no existe
+
+# inicializamos una lista para cada campo que queremos guardar
+id_list = []
+titulo_list = []
+
+
+
 for i in testList:
     for num in itertools.count(1):
-        url="https://www.filmaffinity.com/es/allfilms_" + i + "_" + str(num) + ".html"
+        url = "https://www.filmaffinity.com/es/allfilms_" + i + "_" + str(num) + ".html"
         page = download(url)
         if page is None:
             break
@@ -40,29 +50,43 @@ for i in testList:
             id = re.findall('data-movie-id="(.*?)"', str(page.read()))
             # Descargamos la página de cada película a partir de su identificador
             for j in id:
-                movie = download("https://www.filmaffinity.com/es/film" + str(j) + ".html")
+                movie = download("https://www.filmaffinity.com/es/film" + j + ".html")
                 if page is not None:
                     # (PENDIENTE) Una vez descargada la página extraemos la información de la película
-                     #print(movie.read())
+                    # print(movie.read())
 
                     # Parseamos el código HTML para navegar por las etiquetas
                     soup = BeautifulSoup(movie, 'html.parser')
 
-                    # Vemos el código HTML correctamente 
+                    # Vemos el código HTML correctamente
                     #fixed_html = soup.prettify()
                     #print(fixed_html)
-                    
+
                     # Buscamos entre las etiquetas dl la que tenga la clase movie-info
-                    dl = soup.find('dl', attrs={'class':'movie-info'})
+                    dl = soup.find('dl', attrs={'class': 'movie-info'})
 
                     # Vemos solo la parte de movie-info
                     # print(dl)
 
-                    # Seleccionamos el primer elemento dd
+                     # Seleccionamos el primer elemento dd
                     dd = dl.find('dd')
                     titulo = dd.text.strip()
 
-                    # quitamos 'aka' en los casos en los que existe un alias en el titulo
+                    # quitamos 'aka' en los casos en los que existe en el titulo
                     if titulo[-3:] == 'aka': titulo = titulo[:-3].strip()
 
-                    print(titulo)
+                    print('id: %d , titulo: %s' % (int(j), titulo))
+                    type(j)
+                    
+                    # añadimos a cada una de las listas el dato correspondiente a la ultima pelicula
+                    id_list.append(int(j))
+                    titulo_list.append(titulo)
+                   
+# creamos una dataframe con  todas las listas
+df = pd.DataFrame({'id' : id_list, 'titulo': titulo_list }) 
+
+# guardamos la dataframe a disco
+df.to_csv('filmaffinity.csv', encoding='utf-8')   
+
+
+                    
